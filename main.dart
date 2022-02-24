2ae548843dce453ca299832f95e6c6241a72aad0
rwema3 <rwemadominik3@gmail.com>
2022-02-24 20:58:34 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,4 +8,4 @@ class MyApp extends StatelessWidget {
     // Creamos la Aplicacion
     return new MaterialApp(
       title: "Calculator Flutter",
-      theme: new ThemeData(pri
+      theme: new ThemeData(prim
